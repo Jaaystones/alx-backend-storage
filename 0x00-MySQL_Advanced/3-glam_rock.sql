3da966f8e656d51991f213b6d286a96fac61bcfa
Jay stones <Joelpaul345@gmail.com>
2023-07-13 17:12:59 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- This SQL script select band_name, and lifespan column which is difference
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+-- Query the bands with Glam rock as their main style
+SELECT band_name, (2022 - split(band_formed, '-')[0]) AS lifespan
     FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    WHERE band_style LIKE '%Glam rock%'
     ORDER BY lifespan DESC;
